bff07ad0e0920bf7ede442dff0e398a63171fd2e
AlexMrz <morozovprav@gmail.com>
2023-07-18 22:12:13 +0100

    install tailwind

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import '../css/app.css';
+
 
 createInertiaApp({
   resolve: name => {
