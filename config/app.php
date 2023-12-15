d766e216e615d6080a6023977b0b3f34f87fd743
AlexMrz <morozovprav@gmail.com>
2023-12-15 14:23:34 +0000

    remove unusable package

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-        //PulkitJalan\Google\GoogleServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
